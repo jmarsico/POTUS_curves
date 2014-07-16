f8f8ee88103165e4b8bca118bacaac4149ddf9c2
Jakob Marsico <jmarsico@gmail.com>
2014-07-16 12:00:26 -0400

    changing config.make and main.cpp for rpi branch

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,26 @@
  *
  * Shows how to use ofxTimeline to create a simple curves and color change
  */
-
 #include "ofMain.h"
 #include "testApp.h"
-#include "ofAppGlutWindow.h"
+//#include "ofAppGlutWindow.h"
+#ifdef TARGET_OPENGLES
+#include "ofGLProgrammableRenderer.h"
+#endif
 
 //========================================================================
 int main( ){
 
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+    #ifdef TARGET_OPENGLES
+        ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(new ofGLProgrammableRenderer()));
+        #endif
+   // ofAppGlutWindow window;
+        ofSetupOpenGL(1024,768, OF_WINDOW);                     // <-------- setup the GL context
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new testApp());
+        // this kicks off the running of my app
+        // can be OF_WINDOW or OF_FULLSCREEN
+        // pass in width and height too:
+        ofRunApp( new testApp());
 
 }
+
